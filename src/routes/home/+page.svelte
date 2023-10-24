adc3cbc59604c21c11f0893a07f1eaed9dbcb460
Caio Joseph <caio27joseph@gmail.com>
2023-10-24 09:25:22 -0300

    FIx Bugs

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -8,31 +8,34 @@
 	const bestTable = tables[0];
 </script>
 
-<div
-	class="container h-[80vh] bg-center bg-cover w-full max-w-full flex flex-col justify-evenly"
-	style="background-image: url({bestTable?.imageUrl ?? '/images/table_placeholder.jpg'});"
->
-	<a class="cols grid grid-cols-2" href="/tables/{bestTable.id}">
-		<div class="grow table-info p-8 text-white variant-glass-surface rounded-sm max-w-0.5">
-			<h2 class="text-3xl font-bold mb-4">{bestTable.title}</h2>
-			<p>
-				Aliquip consectetur irure anim laborum officia ea ut sunt excepteur ad consectetur eiusmod.
-				Nostrud occaecat occaecat consectetur incididunt ipsum occaecat est. Exercitation elit
-				excepteur aliquip ullamco laboris sunt ut pariatur aliquip. Consectetur tempor occaecat
-				magna quis est pariatur. Consequat do consequat ipsum laborum qui nisi ipsum ex Lorem ipsum
-				ea. Qui occaecat duis adipisicing voluptate nisi sunt esse commodo adipisicing id voluptate
-				irure deserunt ut.
-			</p>
-			<div class="rating p-10 flex justify-end">
-				<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
-				<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
-				<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
-				<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
-				<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
+{#if bestTable}
+	<!-- content here -->
+	<div
+		class="container h-[80vh] bg-center bg-cover w-full max-w-full flex flex-col justify-evenly"
+		style="background-image: url({bestTable?.imageUrl ?? '/images/table_placeholder.jpg'});"
+	>
+		<a class="cols grid grid-cols-2" href="/tables/{bestTable.id}">
+			<div class="grow table-info p-8 text-white variant-glass-surface rounded-sm max-w-0.5">
+				<h2 class="text-3xl font-bold mb-4">{bestTable.title}</h2>
+				<p>
+					Aliquip consectetur irure anim laborum officia ea ut sunt excepteur ad consectetur
+					eiusmod. Nostrud occaecat occaecat consectetur incididunt ipsum occaecat est. Exercitation
+					elit excepteur aliquip ullamco laboris sunt ut pariatur aliquip. Consectetur tempor
+					occaecat magna quis est pariatur. Consequat do consequat ipsum laborum qui nisi ipsum ex
+					Lorem ipsum ea. Qui occaecat duis adipisicing voluptate nisi sunt esse commodo adipisicing
+					id voluptate irure deserunt ut.
+				</p>
+				<div class="rating p-10 flex justify-end">
+					<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
+					<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
+					<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
+					<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
+					<img class="h-10 w-10" src="/icons/compass.svg" alt="" srcset="" />
+				</div>
 			</div>
-		</div>
-	</a>
-</div>
+		</a>
+	</div>
+{/if}
 <div class="feed flex flex-col space-y-8 -translate-y-44 md:-translate-y-12 lg:-translate-y-32">
 	{#each [1, 2, 3] as _}
 		<!-- This loop is just a placeholder to generate 3 rows. -->
--- src/routes/tables/[tableId]/menu/+page.server.ts
+++ src/routes/tables/[tableId]/menu/+page.server.ts
@@ -68,8 +68,8 @@ export const actions: Actions = {
 		// TODO: error handling
 	},
 	deleteTable: async (event) => {
-		const id = event.params.id;
-		if (!id) {
+		const tableId = event.params.tableId;
+		if (!tableId) {
 			throw redirect(302, '/home');
 		}
 
@@ -86,15 +86,16 @@ export const actions: Actions = {
 		const res = await mutation.mutate(
 			{
 				where: {
-					id
+					id: tableId
 				}
 			},
 			{
 				event
 			}
 		);
-
-		throw redirect(302, '/home');
+		if (res.data?.removeTable) {
+			throw redirect(302, '/home');
+		}
 		// TODO: error handling
 	}
 };
